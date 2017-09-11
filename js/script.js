0d1a55dbbc525126691007835a293b57c1ed3f91
Alvin Niamatali and Hansen Hansen <hansen@>
2017-09-11 09:54:59 -0700

    Two additional functions added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-var conversion = function(gallon) {
+var convGallon = function(gallon) {
   return gallon * 3.78;
 };
 
-var gallon = parseInt(prompt("Enter Gallons:"));
-var result = conversion(gallon);
-alert(result);
+var gallon = parseInt(prompt("Enter Gallons to be convereted to Liters:"));
+var result = convGallon(gallon);
+alert(result+" Liters");
+
+var convMl = function(millileter) {
+  return millileter / 3785.41;
+};
+
+var millileter = parseInt(prompt("Enter Millileters to be converted to Gallons:"));
+var result = convMl(millileter);
+alert(result+" Gallons");
+
+var convMl = function(liters) {
+  return liters * 1000;
+};
+
+var liters = parseInt(prompt("Enter Liters to be converted to Millileters:"));
+var result = convMl(liters);
+alert(result + " Millileters");
